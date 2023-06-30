329e124924bf706131ac90652a6556093131f471
AndersonRamirez037 <andersongamer037@gmail.com>
2023-06-30 12:43:40 -0500

    0. Print list

---
+++ 0-print_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_list - Function that prints all the elements of a list_t list. 
+ * @h: Pointer to the head of the list. 
+ * Return: The number of nodes in the list. 
+*/
+size_t print_list(const list_t *h){
+    size_t nodes = 0; 
+    while (h != NULL) {
+        if (h->str == NULL) {
+            printf("[0] (nill)\n");
+        } else {
+            printf("[%d] %s\n", h->len, h->str);
+        }
+        nodes++;
+        h = h->next; 
+    }
+    return nodes;
+}
